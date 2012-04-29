cb7d5b0689859579ad0df7566bd50085f2a1100b
Jan Lofberg <chenebu@gmail.com>
2012-04-29 13:51:33 +0200

    disclaimer

--- backup.sh
+++ backup.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-# Copyright (c) 2012 jnaO
+# 2012 by jnaO
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is furnished
-# to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, yada yada yada...
+# Use as you please. If you break something using tis program, or
+# if anyone else breaks something for you because of this program
+# dont blame me. Intension is to help you, not to harm you.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# Source is found @ https://github.com/jnaO/file_backup
 #
 
 if [ -e $1 ]
